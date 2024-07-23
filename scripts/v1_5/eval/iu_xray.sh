b0f14158129d7c44a7177f7f853765c633492f75
teddy <teddytw1209@gmail.com>
2024-07-24 01:15:24 +0800

    Revert "change to sbatch"
    This reverts commit 4c639183c72e65c1b63c9197f54a73c3c0a5b652.

--- scripts/v1_5/eval/iu_xray.sh
+++ scripts/v1_5/eval/iu_xray.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=16gb
-#SBATCH --partition=gpu
-#SBATCH --gpus=a100:1
-#SBATCH --time=16:00:00
-#SBATCH --output=%x.%j.out
-
 MODEL_PATH=$1
 CKPT=$2
 CONV_MODE=vicuna_v1
@@ -17,10 +8,7 @@ fi
 
 echo "$MODEL_PATH $CKPT"
 
-module load singularity
-CUDA_VISIBLE_DEVICES=0
-
-singularity exec --nv /blue/bianjiang/tienyuchang/anaconda source activate base; conda activate vila; python -m llava.eval.model_vqa \
+CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_vqa \
     --model-path $MODEL_PATH \
     --question-file ./playground/data/eval/iu_xray/simple_questions.jsonl \
     --image-folder /orange/bianjiang/VLM_dataset/ReportGeneration/IU_X-Ray/Kaggle/images/images_normalized \
