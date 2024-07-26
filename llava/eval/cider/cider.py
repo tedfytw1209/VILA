aa42c36a3a90824d836b72c0677b703b29d9f21b
teddy <teddytw1209@gmail.com>
2024-07-25 18:58:21 -0700

    bug fix

--- llava/eval/cider/cider.py
+++ llava/eval/cider/cider.py
@@ -7,8 +7,7 @@
 #
 # Authors: Ramakrishna Vedantam <vrama91@vt.edu> and Tsung-Yi Lin <tl483@cornell.edu>
 
-from cider_scorer import CiderScorer
-import pdb
+from .cider_scorer import CiderScorer
 
 class Cider:
     """
